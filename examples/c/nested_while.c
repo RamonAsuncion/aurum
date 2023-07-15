5748e06ed1ac002a7bfcb79644b2391d55f3049c
Ramon Asuncion <asuncionbatista@gmail.com>
2023-07-15 13:35:42 -0400

    Remove subfolders from examples/

--- examples/c/nested_while.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
- for (int i = 0; i <= 1; ++i)
-   for (int j = 5; j >= 0; --j)
-     printf("%d\n", j);
- return 0;
-}
--- examples/py/nested_while.py
+++
@@ -1,8 +0,0 @@
-i = 2
-while (i > 0):
-    j = 5
-    while (j > 0):
-        j-=1
-        print(j)
-    i-=1
-
